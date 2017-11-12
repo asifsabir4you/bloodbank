2f664cb8426ac805ca00d37acf21bd2718b1d8c2
Asif Sabir <you@example.com>
2017-11-12 23:34:10 +0600

    settings UI issue update

--- app/src/main/java/com/example/asifsabir/blooddonor/SettingsActivity.java
+++ app/src/main/java/com/example/asifsabir/blooddonor/SettingsActivity.java
@@ -1,6 +1,7 @@
 package com.example.asifsabir.blooddonor;
 
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.ListPreference;
@@ -8,62 +9,41 @@ import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 /**
  * Created by asifsabir on 11/12/17.
  */
 
-public class SettingsActivity extends PreferenceActivity {
+
+public class SettingsActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().setTitle("Settings");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); // Here
+
         if (savedInstanceState == null) {
             getFragmentManager().beginTransaction().replace(android.R.id.content, new MyPreferenceFragment()).commit();
         }
 
     }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home: {
+                Intent intent = new Intent(this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                startActivityIfNeeded(intent, 0);
+                return true;
+            }
+            default:
+                return super.onOptionsItemSelected(item);
+        }
 
+    }
 }
 
-
-//
-//import android.content.Intent;
-//import android.os.Bundle;
-//import android.support.v7.app.AppCompatActivity;
-//import android.support.v7.widget.Toolbar;
-//import android.view.MenuItem;
-//
-//import com.google.firebase.FirebaseApp;
-//
-///**
-// * Created by asifsabir on 11/12/17.
-// */
-//
-//public class SettingsActivity extends AppCompatActivity {
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        FirebaseApp.initializeApp(this);
-//        setContentView(R.layout.activity_settings);
-//        getSupportActionBar().setTitle("Settings");
-//        getSupportActionBar().setDisplayHomeAsUpEnabled(true); // Here
-//
-//    }
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        switch (item.getItemId()) {
-//            case android.R.id.home: {
-//                Intent intent = new Intent(this, MainActivity.class);
-//                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-//                intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
-//                startActivityIfNeeded(intent, 0);
-//                return true;
-//            }
-//            default:
-//                return super.onOptionsItemSelected(item);
-//        }
-//
-//    }
-//
-//}
